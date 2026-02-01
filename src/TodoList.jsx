af88b2f8aa81dbcefd0c905db4d429d54fe1ad5d
Avanish542 <avanishvishwakarma25@gmail.com>
2026-02-01 21:37:43 +0530

    day-5 react-app-todolist-2

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,20 +1,29 @@
 import  "./TodoList.css";
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 
 function TodoList() {
 
-    let [todos, settodos] = useState(["Sample task"]);
+    let [todos, settodos] = useState([{task: "sample-task", id: uuidv4()}]);
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTask = () => {
         console.log("Adding a new task in todo");
-        settodos([...todos, newTodo]);
+        settodos((prevtodos) => {
+            return [...prevtodos, {task: newTodo, id: uuidv4() }]
+        });
         setNewTodo("");
     };
-
+  
     let UpdateTodoValue = (event) => {
         setNewTodo(event.target.value);
+    };
+
+    let deletetodo = (id) => {
+        settodos((prevtodos) => {
+            return prevtodos.filter(todo => todo.id !== id);
+        });
     };
     return ( 
         <div className="todolist"> 
@@ -26,7 +35,11 @@ function TodoList() {
             <h1>Task Todo</h1>
             <ul className="list">
                 {todos.map((todo) =>  {
-                    return <li>{todo}</li>
+                 return <li key={todo.id}>
+                    <span>{todo.task}</span>
+                    &nbsp;  &nbsp;  &nbsp;
+                    <button style={{backgroundColor:"white"}} onClick= {() => deletetodo(todo.id)}>delete</button>
+                </li>
                 })}
             </ul>
         </div>
